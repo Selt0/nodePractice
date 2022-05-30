2b7003ecf905b855c6b50737e5d3742d21b5e886
Michael M <chewedgum12@gmail.com>
2022-05-29 21:08:12 -0500

    Refactored code; keeping HTML dry

--- js/main.js
+++ js/main.js
@@ -1,23 +1,73 @@
-document.querySelector('#spicy').addEventListener('click', makeReq)
-document.querySelector('#meatzerella').addEventListener('click', makeReq)
-document.querySelector('#mushroom').addEventListener('click', makeReq)
+//document.querySelector('#spicy').addEventListener('click', makeReq)
+//document.querySelector('#meatzerella').addEventListener('click', makeReq)
+// document.querySelector('#mushroom').addEventListener('click', makeReq)
+const pizzaIds = []
+renderElements()
+
+document.querySelector('.pizza-list').addEventListener('click', makeReq)
+
+// replace with event delegation, after element render
+
 const tip = document.querySelector('#tip')
 const panel = document.querySelector('.api-content')
 
+
+
+async function renderElements(){
+  // get div that holds the cards
+  let sect = document.getElementById('pizza-list')
+
+  // send value to api
+  const res = await fetch(`/api?data`)
+  //get back data
+  const data = await res.json()
+  console.log(data)
+
+  // for each element, take the name and put it in an h2, take the dispImg and put it in img tag, all inside a card div
+  Object.entries(data).forEach( entry  => {
+
+    const [pizzaId, content] = entry
+    pizzaIds.push(pizzaId)
+    let div = document.createElement('div')
+    div.classList.add('card', 'clickH',pizzaId)
+
+
+    let h2 = document.createElement('h2')
+    h2.textContent = content.name
+    h2.classList.add('clickH', pizzaId)
+
+    let img = document.createElement('img')
+    img.src = content.dispImg
+    img.classList.add('clickH', pizzaId)
+
+    div.appendChild(h2)
+    div.appendChild(img)
+    sect.appendChild(div)
+
+  })
+
+}
+
   // make req
 async function makeReq(e){
+
+  if (!e.target.classList.contains('clickH')) return;
+  console.log(pizzaIds)
   // hide tip
   tip.style.display = "none"
   //show ingredient panel
   panel.style.display = 'initial'
+
   // figure out which pizza user clicked on
-  const pizzaType = e.target.id
+  // if the thing you click on, has a class that is inside pizzaIds, use that classname as the pizzaType
+  const targetClasses = Array.from(e.target.classList)  ;
+  const pizzaType = pizzaIds.filter(element => targetClasses.includes(element))
+  console.log(e)
+  console.log(pizzaType)
   // send value to api
   const res = await fetch(`/api?pizza=${pizzaType}`)
   //get back data
   const data = await res.json()
-
-
 
   //grab ingredients
   let ingredients = data['ingredients']
--- server.js
+++ server.js
@@ -7,6 +7,45 @@ const figlet = require("figlet");
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
+
+  const pizzas = {
+    meatzerella: {
+          name: "Meatzerella",
+          ingredients: [
+            "pepperoni",
+            "ham",
+            "italian sausage",
+            "5-layer cheese",
+          ],
+          dispImg: "https://cache.dominos.com/olo/6_85_6/assets/build/market/US/_en/images/img/products/larges/S_MX.jpg",
+          img: "https://www.bacinos.com/wp-content/uploads/2021/05/27-Meat-Lovers-Pizza-Recipes.jpg",
+        },
+    spicy: {
+          name: "Spicy Fiesta",
+          ingredients: [
+            "jalepenos",
+            "banana peppers",
+            "Roasted red peppers",
+            "pinapple",
+            "pepperoni",
+          ],
+          dispImg: "https://cache.dominos.com/olo/6_85_6/assets/build/market/US/_en/images/img/products/larges/S_PIZPV.jpg",
+          img: "https://skinnyms.com/wp-content/uploads/2012/08/Zucchini-Bell-Pepper-Pizza-1.jpg",
+        },
+    mushroom: {
+          name: "Mushroom Kingdom",
+          ingredients: [
+            "mushroom",
+            "red onion",
+            "Calabrian chiles",
+            "goat cheese",
+          ],
+          dispImg: "https://cache.dominos.com/olo/6_85_6/assets/build/market/US/_en/images/img/products/larges/S_PIZCR.jpg",
+          img: "https://www.pamperedchef.com/iceberg/com/recipe/1684007-lg.jpg",
+        }
+
+  }
+
   console.log(page);
   if (page == "/") {
     fs.readFile("index.html", function (err, data) {
@@ -17,47 +56,14 @@ const server = http.createServer((req, res) => {
   } else if (page == "/api") {
     console.log(params);
     if ("pizza" in params) {
-      if (params["pizza"] == "meatzerella") {
+      if (params["pizza"] in pizzas) {
         res.writeHead(200, { "Content-Type": "application/json" });
-        const objToJson = {
-          ingredients: [
-            "pepperoni",
-            "ham",
-            "italian sausage",
-            "5-layer cheese",
-          ],
-          img: "https://www.bacinos.com/wp-content/uploads/2021/05/27-Meat-Lovers-Pizza-Recipes.jpg",
-        };
-
-        res.end(JSON.stringify(objToJson));
-
-      } else if (params["pizza"] == "spicy") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        const objToJson = {
-          ingredients: [
-            "jalepenos",
-            "banana peppers",
-            "Roasted red peppers",
-            "pinapple",
-            "pepperoni",
-          ],
-          img: "https://skinnyms.com/wp-content/uploads/2012/08/Zucchini-Bell-Pepper-Pizza-1.jpg",
-        };
-        res.end(JSON.stringify(objToJson));
-      } else if (params["pizza"] == "mushroom") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        const objToJson = {
-          ingredients: [
-            "mushroom",
-            "red onion",
-            "Calabrian chiles",
-            "goat cheese",
-          ],
-          img: "https://www.pamperedchef.com/iceberg/com/recipe/1684007-lg.jpg",
-        };
-        res.end(JSON.stringify(objToJson));
+        res.end(JSON.stringify(pizzas[params["pizza"]]));
       }
-    } //student if
+    } else if ("data" in params) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(pizzas));
+      }
   } //else if
   else if (page == "/css/style.css") {
     fs.readFile("css/style.css", function (err, data) {
@@ -82,7 +88,5 @@ const server = http.createServer((req, res) => {
     });
   }
 });
-
-
 
 server.listen(8000);
